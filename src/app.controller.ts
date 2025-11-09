c169340d0ecdab49c572a9b026de7c2fa02e0d6d
Weeraphat2000 <weeraphat.main@gmail.com>
2025-11-09 16:07:18 +0700

    Add Docker configuration, update dependencies, and implement Cat schema with CRUD operations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,34 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cat, CatDocument } from './cat.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @InjectModel(Cat.name) private catModel: Model<CatDocument>,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get('hello/:name')
+  @Get(':name')
   hello(@Param('name') name: string): string {
     return this.appService.hello(name);
+  }
+
+  @Post('create-cat')
+  async createCat(@Body() catData: Cat): Promise<Cat> {
+    console.log('catData', catData);
+    const newCat = new this.catModel({
+      name: 'Whiskers',
+      age: 2,
+      breed: 'Tabby',
+    });
+    return newCat.save();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cat, CatSchema } from './cat.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // โหลด .env
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+        dbName: config.get<string>('DB_NAME', 'nakorncode'),
+      }),
+    }),
+    MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]), // Register Cat schema
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
